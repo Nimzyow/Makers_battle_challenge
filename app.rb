f5164d4e6f87e384a3845f952de47f3c6549bb80
Nimzyow <n_soufiani@hotmail.com>
2020-03-06 12:08:57 +0000

    converted global variables to instance of class

--- app.rb
+++ app.rb
@@ -9,26 +9,43 @@ class Battle < Sinatra::Base
   end
 
   post "/names" do
-    $game = Game.new(Player.new(params[:player_1_name]), Player.new(params[:player_2_name]))
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    @game = Game.create(player_1, player_2)
     redirect(:play)
   end
 
+  before do
+    @game = Game.instance
+  end
+
   get "/play" do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @player_2_hp = $game.player_2.hp
+    @player_1_name = @game.player_1.name
+    @player_2_name = @game.player_2.name
+    @player_2_hp = @game.player_2.hp
     erb(:play)
   end
 
   get "/attack" do
-
-    $game.turn
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @player_1_hp = $game.player_1.hp
-    @player_2_hp = $game.player_2.hp
-    @current_player = $game.current_player.name
+    @player_1_name = @game.player_1.name
+    @player_2_name = @game.player_2.name
+    @player_1_hp = @game.player_1.hp
+    @player_2_hp = @game.player_2.hp
+    @current_player = @game.current_player.name
     erb(:attack)
+  end
+
+  post "/attack" do
+    @game.turn
+    if @game.game_over == true
+      redirect("/game_over")
+   else 
+      redirect("/attack")
+    end
+  end
+
+  get "/game_over" do
+    "game over"
   end
 
   # start the server if ruby file executed directly
--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,43 @@
 class Game
-  attr_reader :player_1, :player_2, :current_player
+  attr_reader :player_1, :player_2, :current_player, :game_over
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
     @current_player = @player_1
     @turn = true
+    @game_over = false
   end
 
   def turn
-    @turn ? attack(@player_2) : attack(@player_1)
+    if @player_1.hp == 0 || @player_2.hp == 0
+      puts "I am stuck"
+    end_game
+    else 
+      puts "I get to attack"
+      @turn ? attack(@player_2) : attack(@player_1) 
+    end
+  end
+
+  # def game_over_check
+  #   if @player_1.hp == 0 || @player_2.hp == 0
+  #     @game_over = @game_over = true
+  #   end
+  # end
+
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
   end
 
   private
 
   def attack(player)
     player.reduce_hp
+    end_game
     switch_current_player
   end
 
@@ -26,5 +48,11 @@ class Game
   def switch_current_player
     @turn ? @current_player = @player_2 : @current_player = @player_1
     switch_turn
+  end
+
+  def end_game
+    if @player_1.hp == 0 || @player_2.hp == 0
+    @game_over = @game_over = true
+    end
   end
 end
